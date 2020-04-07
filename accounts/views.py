f0bbb812f240b0453eaa0160c03e7590048c7fc4
Neil_S <neil.smyth1989@hotmail.com>
2020-04-07 20:09:57 +0000

    added logout functionality to index via views and url plus settings.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, reverse
+from django.contrib import auth, messages
 
 
 def say_hello(request):
@@ -7,3 +8,10 @@ def say_hello(request):
 
 def index(request):
     return render(request,  'index.html')
+
+
+def logout(request):
+    auth.logout(request)  # request contains user object
+    messages.success(request, "You are now logged out!")
+    return redirect(reverse('index'))  
+    # pass in name of url we want to be redirect to
--- machine/urls.py
+++ machine/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from machine.views import current_datetime, hours_ahead
-from accounts.views import say_hello, index
+from accounts.views import say_hello, index, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index),
+    url(r'^$', index, name='index'),
     url(r'^hello/', say_hello),  # renders landing/base page
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^accounts/logout/$', logout, name='logout')  #name maps to url at href
     # 'We’ve curtailed the outlandishness here by limiting
     # the offset to 99 hour' (pg 60)
 
