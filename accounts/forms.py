5fd5f283e60b581994aea2ae4b959b56f75bbfb0
Neil_S <neil.smyth1989@hotmail.com>
2020-04-19 21:09:07 +0000

    added accounts forms.py logic

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,18 +14,51 @@ class UserLoginForm(forms.Form):  # Model forms
 
 class UserCreationForm(UserCreationForm):  # as arg
 
+    # username = forms.CharField(
+    #     label="Username",
+    #     min_length=4,
+    #     max_length=20,
+    #     widget=forms.TextInput, required=True)
+    
     password1 = forms.CharField(
         label="Password",
+        min_length=4,
+        max_length=20,
         widget=forms.PasswordInput)
              
     password2 = forms.CharField(
         label="Password Confirmation",
+        min_length=4,
+        max_length=20,
         widget=forms.PasswordInput)
 
     class Meta:  # inner class provides info about blueprint form
         model = User  # we want to store info in
         fields = ['email', 'username', 'password1', 'password2']
 
+
+    def clean_email(self):
+
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+       
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'Email address has to be unique')
+      
+        return email
+
+    def clean_password2(self):
+
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2:
+            raise ValidationError("Please confirm your password")
+      
+        if password1 != password2:
+            raise ValidationError("Passwords have to match")
+        
+        return password2
 
     # def clean_input(self):
     #     cleaned_data = super(UserCreationForm, self).clean()
