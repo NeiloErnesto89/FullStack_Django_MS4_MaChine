cbf0ebed4f6105c51b8d22e5142a541bb852c83b
Neil_S <neil.smyth1989@hotmail.com>
2020-04-26 22:05:40 +0000

    added a url accounts file and reset

--- machine/urls.py
+++ machine/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from machine.views import current_datetime, hours_ahead
-from accounts.views import say_hello, index, logout, login, registration, user_profile
+from accounts.views import say_hello, index
+from accounts import urls as accounts_urls
 
 """
     # name maps to url at href
@@ -31,9 +32,6 @@ urlpatterns = [
     url(r'^hello/', say_hello),  # renders landing/base page
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    url(r'^accounts/login/$', login, name='login'),
-    url(r'^accounts/logout/$', logout, name='logout'),
-    url(r'^accounts/register/$', registration, name='registration'),
-    url(r'^accounts/profile/$', user_profile, name="profile")
+    url(r'^accounts/', include(accounts_urls))
 
 ]
